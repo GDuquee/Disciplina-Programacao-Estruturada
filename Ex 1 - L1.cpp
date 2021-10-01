1a4c6fe40f074ab2348de7f825015a5f51b94b1a
Gabriel Duque da Silva <79943080+GDuquee@users.noreply.github.com>
2021-09-30 22:30:38 -0300

    Update Ex 1 - L1.cpp

--- Ex 1 - L1.cpp
+++ Ex 1 - L1.cpp
@@ -1,34 +1,25 @@
 #include<stdio.h>
+//Escreva um prgrama que declare um vetor de reais e leia as notas de 30 alunos
 int main(){
 	
 	//---------------Variaveis--------------//
 	float notasAlunos[3];
 	int i;
 	
-	//---------------Repetiçao--------------//
+	//---------------RepetiÃ§ao--------------//
 	for(i=0; i<3; i++)
 	{
 		printf("Nos informe a sua nota:");
 		scanf("%f", &notasAlunos[i]);
 	}
 		
-	//---------------------Estrutura de Condição-------------------//
-	//---------------------Relatório-------------------//
-	printf("				Relatorio de Notas \n\n");
+	//---------------------Estrutura de CondiÃ§Ã£o-------------------//
+	//---------------------RelatÃ³rio-------------------//
+	printf("				Relatorio de Notas do Alunos \n\n");
 	for(i=0; i<3; i++)
 	{
 		printf("%.1f \n\n", notasAlunos[i]);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	return 0;
 }
