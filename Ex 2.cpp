8d100ff122a552aacbff7d0a0ce741f910ec6ed0
Gabriel Duque da Silva <79943080+GDuquee@users.noreply.github.com>
2022-03-06 20:21:28 -0300

    Add files via upload

---
+++ Ex 2.cpp
@@ -0,0 +1,24 @@
+// Sintese
+// Objetivo : Receber nome e idade de um aluno;
+// Entrada : Idade e nome;
+// Saída : Idade e nome;
+#include <stdio.h>
+#include <stdlib.h>
+
+struct DadosAluno {
+  char nome[20];
+  int idade;
+};
+
+int main() {
+  struct DadosAluno dados;
+  printf("Digite o nome do aluno: \n");
+gets(dados.nome);
+  printf("Digite a idade do aluno: \n");
+  scanf("%i", &dados.idade);
+  system("cls");
+  printf("Nome do aluno: %s\nIdade do aluno: %i", dados.nome, dados.idade);
+  return 0;
+}
+© 2021 GitHub, Inc.
+Ter
